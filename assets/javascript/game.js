fad263be55e22689516c10198766c5859e3ee6cb
Mario Miramontes <mmisthree@gmail.com>
2018-11-19 22:18:27 -0800

    gave a number value to each image in js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,57 +7,37 @@ var gemValues = [];
 console.log(gemValues)
 
 
-
+$(document).ready(function () {
 
 // sets the score you need to match
 $("#computer-score").text(compNum);
 
+
+
 //Generates 4 random numbers and puts them in gemValues array
+function generateGemNumber() {
 for (var i = 0; i < 4; i++) {
     var gemRandNum = Math.floor(Math.random() *12 + 1);
     gemValues.push(gemRandNum);  
 }
-
-//assigns a value from gemValues[] to the gem images
-for (var i=0; i < gemValues.length; i++) {
-    $(".crystal");
 }
 
+//assigns a value from gemValues[] to the gem images and makes them clickable
+generateGemNumber();
 
-//gives each crystal image a number from gemValues array
-for( i=0; i<gemValues.length; i++) {
-    $(".crystals").data("value");
-}
+    $('img').each(function (index) {
+        $(this).attr('data-crystal', gemValues[index]);
+        $("#user-score").text($(this).attr("data-crystal",));
 
-
-
-
-
+    })
+    
+    $('img').on('click', function(){
+        console.log($(this).attr('data-crystal'));
+    });
+    
+ 
 
 
 //write's the gem value to the page
-// $("#gem-one").on("click", function() {
-//     var gemOneAdd = document.getElementById("user-score").innerHTML = gemValues[0];
-// });
-// $("#gem-two").on("click", function() {
-//     var gemTwoAdd = document.getElementById("user-score").innerHTML = gemValues[1];
-// });
-// $("#gem-three").on("click", function() {
-//     var gemThreeAdd = document.getElementById("user-score").innerHTML = gemValues[2];
-// });
-// $("#gem-four").on("click", function() {
-//     var gemFourAdd = document.getElementById("user-score").innerHTML = gemValues[3];
-// });
-    
 
-
-// var gemOneAdd = $("#gem-one") = gemValues[0];
-// console.log(gemOneAdd)
-
-
-// $("img").on("click", function(){
-//     for (var i=0; i<gemValues.length; i++){
-//         console.log(gemvaules)
-//     }
-
-// });
+})
